b76bfd831e2377c2be972c3715f3d22b80375025
amir pouran ben veyseh <apouranb@legendary1.cs.uoregon.edu>
2019-10-20 19:37:08 -0700

    a

--- models/asgcn.py
+++ models/asgcn.py
@@ -8,6 +8,29 @@ from layers.dynamic_rnn import DynamicLSTM
 
 INFINITY_NUMBER = 1e12
 
+class GraphConvolution(nn.Module):
+    """
+    Simple GCN layer, similar to https://arxiv.org/abs/1609.02907
+    """
+    def __init__(self, in_features, out_features, bias=True):
+        super(GraphConvolution, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = nn.Parameter(torch.FloatTensor(in_features, out_features))
+        if bias:
+            self.bias = nn.Parameter(torch.FloatTensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+
+    def forward(self, text, adj):
+        hidden = torch.matmul(text, self.weight)
+        denom = torch.sum(adj, dim=2, keepdim=True) + 1
+        output = torch.matmul(adj, hidden) / denom
+        if self.bias is not None:
+            return output + self.bias
+        else:
+            return output
+
 class ASGCN(nn.Module):
     def __init__(self, embedding_matrix, opt):
         super(ASGCN, self).__init__()
