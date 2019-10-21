786e446e86333737d9a70efcd8bd52f4f81c3308
amir pouran ben veyseh <apouranb@legendary1.cs.uoregon.edu>
2019-10-20 23:35:18 -0700

    a

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 SAVE_ID=$1
-CUDA_VISIBLE_DEVICES=0 python train.py --model_name asgcn --dataset rest14 --save True --save_name $SAVE_ID
+CUDA_VISIBLE_DEVICES=0 python train.py --model_name asgcn --dataset lap14 --save True --save_name $SAVE_ID
